de6bf43cce69492a7373e31fd8cb20c3f3d48815
Konrad Lindebjerg <konradlindebjerg@gmail.com>
2026-06-02 22:15:55 +0200

    Added scafolding for manage foods

--- blueprints/foods/__init__.py
+++ blueprints/foods/__init__.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, session
+    Blueprint, render_template, request, redirect, url_for, flash, session
 )
 
 from core.extensions import db
@@ -16,3 +16,14 @@ def manage_foods():
 
     return render_template('manage_foods.html', foods=foods)
     
+@foods_bp.route('/manage_foods/delete_food', methods=['POST'])
+def delete_food():
+    return redirect(url_for('foods_bp.manage_foods'))
+
+@foods_bp.route('/manage_foods/update_food', methods=['POST'])
+def update_food():
+    return redirect(url_for('foods_bp.manage_foods'))
+
+
+
+
