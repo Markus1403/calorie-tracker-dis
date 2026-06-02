ab8119fa66879ff7490ebd326e0d8ca56548b719
Huubinh <qkj830@alumni.ku.dk>
2026-06-02 19:54:12 +0200

    Regex for passwords

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -8,6 +8,8 @@ from core.extensions import db
 from sqlalchemy import text
 from core.models import Food, User, FoodLog, CalorieProfile
 
+import re
+
 main_bp = Blueprint('main_bp', __name__)
 
 
@@ -60,10 +62,25 @@ def register():
             flash('That username is already taken.')
         else:
             user = User(username=input_username, password=password)
-            db.session.add(user)
-            db.session.commit()
-            insert_standard_foods(user.id)
-            return redirect(url_for('main_bp.login'))
+            pattern = r"[`~!@#$%^&*()_\-=+\[{\]}\\|;:'\",<.>/?]"
+            special_check = re.findall(pattern, user.password)
+            pattern = r"[A-Z]"
+            capital_check = re.findall(pattern, user.password)
+            final_check = True
+            if len(password) < 5: 
+                flash('Length of password must be atleast 5 characters')
+                final_check = False
+            if not special_check:
+                flash('Password must contain atleast 1 special character')
+                final_check = False
+            if not capital_check:
+                flash('Password must contain atleast 1 capital character')
+                final_check = False
+            if final_check == True:
+                db.session.add(user)
+                db.session.commit()
+                insert_standard_foods(user.id)
+                return redirect(url_for('main_bp.login'))
 
     return render_template('register.html')
 
