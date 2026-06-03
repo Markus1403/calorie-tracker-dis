c6e8a0227fcd7926f5c4ab0dbb2c5756fee4cc40
RedXin20 <146957816+RedXin20@users.noreply.github.com>
2026-06-03 14:34:48 +0200

    new food items
    You can now upload new foods into the list, and the SQL should handle
    everything :)

--- blueprints/foods/__init__.py
+++ blueprints/foods/__init__.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, render_template, request, redirect, url_for, flash, session
 )
 
+from core.helpers import calculate_calories
 from core.extensions import db
 from core.models import Food
 from sqlalchemy import text
@@ -32,7 +33,6 @@ def delete_food():
         }
     )
     db.session.commit()
-
     return redirect(url_for('foods_bp.manage_foods'))
 
 @foods_bp.route('/manage_foods/update_food', methods=['POST'])
@@ -63,9 +63,33 @@ def update_food():
         }
     )
     db.session.commit()
-
     return redirect(url_for('foods_bp.manage_foods'))
 
 
+@foods_bp.route('/manage_foods/insert_food', methods=['POST'])
+def insert_food():
+    food_id = request.form.get("food_id")
+    name = request.form.get("name")
+    protein = request.form.get("protein")
+    carbs = request.form.get("carbs")
+    fat = request.form.get("fat")
 
+    calories = calculate_calories(carbs, fat, protein)
 
+    db.session.execute(
+            text("""
+                INSERT INTO foods (user_id, name, protein, carbs, fat, calories)
+                VALUES (:user_id, :name, :protein, :carbs, :fat, :calories)
+            """),
+            {
+                "food_id": food_id,
+                "user_id": session["user_id"],
+                "name": name,
+                "carbs": carbs,
+                "protein": protein,
+                "fat": fat,
+                "calories": calories
+            }
+        )
+    db.session.commit()
+    return redirect(url_for('foods_bp.manage_foods'))
